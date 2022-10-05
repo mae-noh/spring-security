023d0f3d18fb3e7ef7dec85f6228fece44901248
MyeongheeNoh <hoppindkfgdl@gmail.com>
2022-10-05 22:56:54 +0900

    Initial commit   - Add user and authority   - Web ignore setting

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,44 +1,58 @@
 package io.security.corespringsecurity.security.configs;
 
-import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
-@Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // 사용자 추가, 권한 추가
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
         String password = passwordEncoder().encode("1111");
+
         auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-        auth.inMemoryAuthentication().withUser("manager").password(password).roles("USER","MANAGER");
-        auth.inMemoryAuthentication().withUser("admin").password(password).roles("USER","MANAGER","ADMIN");
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "USER", "MANAGER"); // 계층구조 권한 적용 전
     }
 
+    // 평문 비밀번호 암호화
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // webignore 설정
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations()); // 정적파일의 경우, 보안 필터를 거치지 않고 통과되도록
     }
 
     @Override
-    protected void configure(final HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/").permitAll() // webingnore와 비슷하지만, 보안필터를 거침
                 .antMatchers("/mypage").hasRole("USER")
                 .antMatchers("/messages").hasRole("MANAGER")
                 .antMatchers("/config").hasRole("ADMIN")
-                .anyRequest().authenticated()
+                .anyRequest().authenticated() // 시스템에 접속하기 위해서는 인증을 받아야함
 
         .and()
-                .formLogin();
+                .formLogin() // 인증방식 formLogin()
+        ;
     }
+
 }
